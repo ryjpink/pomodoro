eaf216a58c39e7882a0abe72f95d237fbc94bcda
Ya-Juan Ruan <loveyou520she@gmail.com>
2022-07-16 18:10:44 -0700

    Add todo list

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ const startButton = document.getElementById("start-button")
 const stopButton = document.getElementById("stop-button")
 const timerDuration = document.getElementById("timer-duration")
 const countDown = document.getElementById("countdown")
+const taskList = document.getElementById("task-list")
+const addTaskButton = document.getElementById("add-task")
+const taskContent = document.getElementById("task-content")
+const tasks = []
+
+let taskIdCounter = 0
 let timeoutId = null
 
 const wakeUp = () => {
@@ -19,7 +25,7 @@ const wakeUp = () => {
 }
 
 const startTimer = () => {
-    const totalTimeMs = timerDuration.value*1000
+    const totalTimeMs = timerDuration.value * 1000
     const endTime = Date.now() + totalTimeMs
     const updateCountdown = () => {
         const timeRemainingMs = endTime - Date.now()
@@ -41,7 +47,6 @@ const startTimer = () => {
 
     startButton.hidden = true
     stopButton.hidden = false
-    
 }
 
 const stopTimer = () => {
@@ -57,3 +62,73 @@ const stopTimer = () => {
 
 startButton.addEventListener("click", startTimer)
 stopButton.addEventListener("click", stopTimer)
+timerDuration.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        if (startButton.hidden) {
+            stopTimer()
+        } else {
+            startTimer()
+        }
+    }
+})
+
+const addTask = (newTask) => {
+    const newLine = document.createElement("li")
+    newLine.classList.add("task")
+    taskList.appendChild(newLine)
+    const task = document.createElement("div")
+    task.contentEditable = true
+    task.addEventListener("blur", () => updateTask(newTask.id, task.innerText))
+    task.innerText = newTask.text
+    const checkBox = document.createElement("input")
+    checkBox.type = "checkbox"
+    const spacer = document.createElement("div")
+    newLine.appendChild(task)
+    spacer.classList.add("spacer")
+    newLine.appendChild(spacer)
+    newLine.appendChild(checkBox)
+    taskContent.value = ""
+}
+
+const createTask = () => {
+    const text = taskContent.value
+    if (text !== "") {
+        const newTask = { 'id': taskIdCounter, 'text': text }
+        taskIdCounter += 1
+        tasks.push(newTask)
+        saveTasks()
+        addTask(newTask)
+    }
+}
+
+const saveTasks = () => {
+    console.log("saving")
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+}
+
+const updateTask = (taskId, newText) => {
+    for (const item of tasks) {
+        if (item.id === taskId) {
+            item.text = newText
+            break
+        }
+    }
+
+    saveTasks()
+}
+
+addTaskButton.addEventListener("click", createTask)
+taskContent.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        createTask()
+    }
+})
+
+
+const loadedTasks = localStorage.getItem("tasks")
+if (loadedTasks !== null) {
+    tasks.push(...JSON.parse(loadedTasks))
+    for (const task of tasks) {
+        addTask(task)
+    }
+}
